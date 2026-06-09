7e3e83d1fd559b94650c358f120491af50c54777
Aayush7105 <aayushrawat5107@gmail.com>
2026-06-09 15:11:38 +0530

    [Aayush Rawat] Add: <Added and solved a scenario based Question>

--- core-java-practice/gcr-codebase/programming-elements/FeetToYardsAndMiles.java
+++ core-java-practice/gcr-codebase/programming-elements/FeetToYardsAndMiles.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DistanceConverter {
+public class FeetToYardsAndMiles {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
